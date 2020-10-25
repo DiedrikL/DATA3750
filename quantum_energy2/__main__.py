28443f39f6bed7dead8be9662c2c2bbf9275f391
AleksR <aleksander.roev@gmail.com>
2020-10-25 18:52:49 +0100

    Removed superflous statement

--- quantum_energy2/__main__.py
+++ quantum_energy2/__main__.py
@@ -2,8 +2,8 @@ import sys
 import quantum_energy2.estimate_ground_state as estimate
 from quantum_energy2.utils import parse_cli_arguments, parse_config_file
 
+# Retrieves arguments either from command line or from config.ini
+args = parse_cli_arguments() if (len(sys.argv) > 1) else parse_config_file()
 
-if __name__ == '__main__':
-    
-    args = parse_cli_arguments() if (len(sys.argv) > 1) else parse_config_file()
-    estimate.run(args = args, num_particles = args['num_particles'])
+# Estimates the ground state with the given arguments
+estimate.run(args = args, num_particles = args['num_particles'])
